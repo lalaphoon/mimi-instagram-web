7a23d3eb56a154401c6c315757279ac59289c7d7
Monica Luo <monica.luo@zemind.ca>
2020-01-27 22:29:10 -0500

    Added tabs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import '../styles/Home.css';
 
+import { Tabs, Button } from 'antd';
+const { TabPane } = Tabs;
+
 export function Home() {
-    return (<div>Home</div>);
+    const operations = <Button>Create New Post</Button>;
+    return (
+        <Tabs tabBarExtraContent={operations} className={"main-tabs"}>
+            <TabPane tab="Image Posts" key="1">
+                Content of tab 1
+            </TabPane>
+            <TabPane tab="Video Posts" key="2">
+                Content of tab 2
+            </TabPane>
+            <TabPane tab="Map" key="3">
+                Content of tab 3
+            </TabPane>
+        </Tabs>
+            );
 };
