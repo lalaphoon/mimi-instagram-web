b86a6634e54262e47eb3a7bef557f49e3c7742e5
Monica Luo <monica.luo@zemind.ca>
2020-02-03 22:39:18 -0500

    Be able to open/close the marker info

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -4,18 +4,36 @@ import {
     withGoogleMap,
     GoogleMap,
     Marker,
+    InfoWindow,
 } from "react-google-maps";
 
 class NormalAroundMap extends React.Component {
+    state = {
+        isOpen : true,
+    }
+
+    onToggleOpen = () => {
+        this.setState(({isOpen}) => ({
+            isOpen: !isOpen,
+        }));
+    }
+
     render() {
         return (
-            <GoogleMap
-                defaultZoom={8}
-                defaultCenter={{ lat: -34.397, lng: 150.644 }}
-            >
+            <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
                 <Marker
                     position={{ lat: -34.397, lng: 150.644 }}
-                />
+                    onClick={this.onToggleOpen}>
+                    {(
+                        this.state.isOpen ?
+                            (
+                                <InfoWindow onCloseClick={this.onToggleOpen}>
+                                    <div>InfoWindow</div>
+                                </InfoWindow>
+                            ) :
+                            null
+                    )}
+                </Marker>
             </GoogleMap>
         );
     }
